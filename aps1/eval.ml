38e9f4897eec6f561b40314cf935f0ae4daeb427
XAVIER JULIEN <3521024@ppti-14-502-07.ufr-info-p6.jussieu.fr>
2019-03-27 19:44:03 +0100

    APS1 eval à debug

--- aps1/eval.ml
+++ aps1/eval.ml
@@ -7,12 +7,13 @@ type valeur = InN of int
 			| InFR of string * valeur
 			| InA of string
 			| InP of block * string list * (string * valeur) list
+			| InPR of string * valeur
 
 let cpt = ref 0
 let alloc mem =
-	let a = "a"^!cpt in
+	let a = "a"^(string_of_int !cpt) in
 		cpt:=(!cpt+1);
-		(a,((a,-1)::mem))
+		(a,(a,ref (InN(-1)))::mem)
 
 let get_int v =
 	match v with
@@ -50,65 +51,90 @@ and parse_argfin arg =
 		match arg with
 		|ASTArgFin(id,t) -> id::[]
 
-let rec eval_args env args =
+
+let rec eval_args env mem args =
 	match args with
-		|ASTExpr(a) ->  (eval_expr env a)::[]
-		|ASTExprs(a,abis) -> (eval_expr env a)::(eval_args env abis)
+		|ASTExpr(a) ->  (eval_expr env mem a)::[]
+		|ASTExprs(a,abis) -> (eval_expr env mem a)::(eval_args env mem abis)
 
 and eval_cmds env mem s ast =
 	match ast with
 	|ASTStat(stat) -> eval_stat env mem s stat
 	|ASTDec(dec,cmds) -> let (new_env,new_mem) = eval_dec env mem dec in eval_cmds new_env new_mem s cmds
-	|ASTStats(stat,cmds) -> let new_mem = eval_stat env mem s stat; eval_cmds env new_mem s cmds
+	|ASTStats(stat,cmds) -> let new_mem = eval_stat env mem s stat in eval_cmds env new_mem s cmds
+
+and eval_block env mem s ast =
+	match ast with
+	|ASTBlock(cmds) -> eval_cmds env mem s cmds
 
 and eval_stat env mem s ast =
 	match ast with
-	|ASTEcho(e) -> let res = eval_expr env mem e in s:=!s^(get_string res)^"\n"
+	|ASTEcho(e) -> let res = eval_expr env mem e in s:=!s^(get_string res)^"\n";(mem,s)
+	|ASTSet(id,e) -> (match List.assoc id env with 
+					    InA(a)-> let v = (List.assoc a mem) in
+					 			   let affect = eval_expr env mem e in 
+					 		    	 v:= affect;(mem,s)
+					 	|_ -> failwith "Error set : not a InA")
+	|ASTBIf(e,b1,b2) -> if (eval_expr env e) = InN(1) then (eval_block env mem b1) else (eval_expr env mem b2)
+	|ASTWhile(e,b) -> if (eval_expr env e1) = InN(0) then (mem,s) else let (new_mem,new_s) = (eval_block env mem b) in eval_stat env new_mem new_s ast
+	|ASTCall(p,args) -> let eval_p = eval_expr env mem p  and args_list = eval_args env mem args in
+												(match eval_p with
+													|InP(block,params,env1) -> let closure_env = (List.map2 (fun x y -> (x,y)) params args_list)@env1 in
+																												eval_block closure_env mem s block
+													|InPR(p,InP(block,params,env1)) -> let closure_env =
+																								(p,List.assoc p env)::(List.map2 (fun x y -> (x,y)) params args_list)@env1 in
+													 															eval_block closure_env mem s block
+													|_ -> failwith "erreur : impossible d'appliquer une valeur entière")
+
 
 and eval_dec env mem ast =
 	match ast with
-	|ASTConst(id,t,e) -> let v = eval_expr env e in  (id,v)::env
-	|ASTFun(id,t,args,e) -> (id,InF(e,parse_args args,env))::env
+	|ASTConst(id,t,e) -> let v = eval_expr env e in  ((id,v)::env,mem)
+	|ASTFun(id,t,args,e) -> ((id,InF(e,parse_args args,env))::env,mem)
 	|ASTFunRec(id,t,args,e) -> let params = parse_args args in
-								 (id,InFR(id,InF(e,params,env)))::env
+								 ((id,InFR(id,InF(e,params,env)))::env,mem)
+	(*aps1*)
 	|ASTVar(id,t) -> let (a,new_mem) = alloc(mem) in ((id,InA(a))::env,new_mem)
-	|ASTProc(id,args,e) -> (id,InF(e,parse_args args,env))::env)
-	|ASTProcRec(id,args,e) -> let params = parse_args args in
-								 (id,InFR(id,InF(e,params,env)))::env
+	|ASTProc(id,args,b) -> (((id,InP(b,parse_args args,env))::env),mem)
+	|ASTProcRec(id,args,b) -> let params = parse_args args in
+								 ((id,InPR(id,InP(b,params,env)))::env,mem)
 
 
-and eval_expr env ast =
+and eval_expr env mem ast =
 	match ast with
 	ASTTrue -> InN(1)
 	|ASTFalse -> InN(0)
 	|ASTNum(n) -> InN(n)
-	|ASTId(id) -> (List.assoc id env)
+	|ASTId(id) -> (match (List.assoc id env) with 
+				  |InA(a) -> !(List.assoc a mem)
+				  |v -> v)
 	|ASTLambda(args,e) -> InF(e,parse_args args,env)
-	|ASTIf(e1,e2,e3) -> if (eval_expr env e1) = InN(1) then (eval_expr env e2) else (eval_expr env e3)
-	|ASTApply(e,args) -> let eval_e = eval_expr env e  and args_list = eval_args env args in
+	|ASTIf(e1,e2,e3) -> if (eval_expr env mem e1) = InN(1) then (eval_expr env mem e2) else (eval_expr env mem e3)
+	|ASTApply(e,args) -> let eval_e = eval_expr env mem e  and args_list = eval_args env mem args in
 												(match eval_e with
 													|InF(body,params,env1) -> let closure_env = (List.map2 (fun x y -> (x,y)) params args_list)@env1 in
-																												eval_expr closure_env body
+																												eval_expr closure_env mem body
 													|InFR(f,InF(body,params,env1)) -> let closure_env =
 																								(f,List.assoc f env)::(List.map2 (fun x y -> (x,y)) params args_list)@env1 in
-													 															eval_expr closure_env body
+													 															eval_expr closure_env mem body
 													|_ -> failwith "erreur : impossible d'appliquer une valeur entière")
 	|ASTOprim(oprim) -> match oprim with
-											|ASTUnary(opun,arg) ->
-													(match opun with
-													|Not -> InN(pi_un "not" (get_int (eval_expr env arg)))
-													)
-											|ASTBinary(opbin,arg1,arg2) ->
-																	let a1 = get_int (eval_expr env arg1) and a2 = get_int (eval_expr env arg2) in
-																		match opbin with
-																		| And -> InN(pi_bin "and" a1 a2)
-																		| Or -> InN(pi_bin "or" a1 a2)
-																		| Eq ->  InN(pi_bin "eq" a1 a2)
-																		| Lt ->  InN(pi_bin "lt" a1 a2)
-																		| Add -> InN(pi_bin "add" a1 a2)
-																		| Sub -> InN(pi_bin "sub" a1 a2)
-																		| Mul -> InN(pi_bin "mul" a1 a2)
-																		| Div -> InN(pi_bin "div" a1 a2)
+						|ASTUnary(opun,arg) ->
+								(match opun with
+								|Not -> InN(pi_un "not" (get_int (eval_expr env mem arg)))
+								)
+						|ASTBinary(opbin,arg1,arg2) ->
+												let a1 = get_int (eval_expr env mem arg1) and a2 = get_int (eval_expr env mem arg2) in
+													match opbin with
+													| And -> InN(pi_bin "and" a1 a2)
+													| Or -> InN(pi_bin "or" a1 a2)
+													| Eq ->  InN(pi_bin "eq" a1 a2)
+													| Lt ->  InN(pi_bin "lt" a1 a2)
+													| Add -> InN(pi_bin "add" a1 a2)
+													| Sub -> InN(pi_bin "sub" a1 a2)
+													| Mul -> InN(pi_bin "mul" a1 a2)
+													| Div -> InN(pi_bin "div" a1 a2)
+
 
 let eval_prog ast =
 	match ast with
